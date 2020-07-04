039d018187a8af97383e8c19077686fc7b77bf85
Anjan Roy <anjanroy@yandex.com>
2020-07-04 08:41:49 +0530

    new: connects to websocket endpoint

--- mgs/getWeb3.py
+++ mgs/getWeb3.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+from __future__ import annotations
 from web3 import Web3, HTTPProvider, WebsocketProvider
 
 
@@ -23,6 +24,28 @@ def connectToHTTPEndPoint(protocol: str, hostname: str, port: str) -> Web3:
         return None
 
     return Web3(HTTPProvider('{}://{}:{}'.format(protocol, hostname, port)))
+
+
+def connectToWebSocketEndPoint(protocol: str, hostname: str, port: str) -> Web3:
+    '''
+        Connects to an WebSocket RPC endpoint, all args are required
+
+        - protocol must be one of {ws, wss}
+
+        - port needs to be >=1 && <2^16
+
+        - returns a web3 connection
+    '''
+    if not (protocol and hostname and port):
+        return None
+
+    if protocol not in ['ws', 'wss']:
+        return None
+
+    if port not in range(1, 2**16):
+        return None
+
+    return Web3(WebsocketProvider('{}://{}:{}'.format(protocol, hostname, port)))
 
 
 if __name__ == '__main__':
