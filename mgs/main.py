77daa4022558fd183bd347e33ac02d64f0f6016a
Anjan Roy <anjanroy@yandex.com>
2020-07-06 15:52:21 +0530

    new: initializing all data items, at very beginning, fetching last `X` block data

--- mgs/main.py
+++ mgs/main.py
@@ -1,6 +1,43 @@
 #!/usr/bin/python3
 
 from __future__ import annotations
+from pandas import DataFrame
+from web3 import Web3
+from typing import Dict, Tuple
+from .util import (
+    processBlockTransactions,
+    processBlockData
+)
+
+
+def init(block: int, config: Dict[str, int], x: int, provider: Web3) -> Tuple[DataFrame, DataFrame]:
+    allTx = DataFrame()
+    blockData = DataFrame()
+
+    print("[+]MATIC Gas Station\n")
+    print("\n[*]Safelow = {} % of blocks accepting. Usually confirms in less than 30min.".format(
+        config.get('safelow')))
+    print("\n[*]Standard = {} % of blocks accepting. Usually confirms in less than 5min.".format(
+        config.get('standard')))
+    print("\n[*]Fast = {} % of blocks accepting. Usually confirms in less than 1min.".format(
+        config.get('fast')))
+    print("\n[*]Fastest = all blocks accepting.  As fast as possible but you are probably overpaying.")
+
+    print("\n[*]Now loading gasprice data from last {} blocks...give me a minute".format(x))
+
+    for pastblock in range((block - x), (block), 1):
+        (mined_blockdf, block_obj) = processBlockTransactions(pastblock, provider)
+
+        allTx = allTx.combine_first(mined_blockdf)
+
+        block_sumdf = processBlockData(mined_blockdf, block_obj)
+        blockData = blockData.append(block_sumdf, ignore_index=True)
+
+    print("Done. now reporting gasprice recs in gwei: \n")
+    print("\npress ctrl-c at any time to stop monitoring\n")
+    print("**** And the oracle says...**** \n")
+
+    return allTx, blockData
 
 
 def main():
