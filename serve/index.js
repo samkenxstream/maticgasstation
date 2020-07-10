1e310afefc911b8ea1031070a0c35ded4e832456
Anjan Roy <anjanroy@yandex.com>
2020-07-10 08:52:03 +0530

    new: simple nodejs application for serving gas station recommended prices to devs

---
+++ serve/index.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const createServer = require('http').createServer;
+const exists = require('fs').exists;
+
+// put absolute path to target file
+const TARGETFILE = '';
+const PORT = 8000;
+const HOST = '0.0.0.0';
+
+const app = require('express')();
+
+/**
+ * Simple traffic logger, may be useful in future, can be retrieved using
+ * journalctl, if deployed using systemd
+ */
+app.use((req, res, next) => {
+    console.log(`${req.method} | ${req.url} | ${req.ip} | ${Date().toString()}`);
+    next();
+});
+
+/**
+ * Our simple server is going to serve on this path, attempting to access
+ * any other, simply returns 404
+ * 
+ * Response will be of JSON type.
+ */
+app.get('/', (req, res) => {
+
+    exists(TARGETFILE, (v) => {
+
+        if (v) {
+
+            res.status(200).type('application/json').sendFile(TARGETFILE, (err) => {
+                if (err)
+                    res.status(404).end();
+            });
+
+        } else {
+
+            res.status(404).json({ msg: "not available" });
+
+        }
+
+    });
+
+});
+
+/**
+ * For any other path, returns 404
+ */
+app.get('*', (req, res) => {
+
+    res.status(404).json({ msg: 'bad path' });
+
+});
+
+createServer(app).listen(PORT, HOST, () => {
+    console.log(`[+]HTTP Server running on http://${HOST}:${PORT}`);
+});
