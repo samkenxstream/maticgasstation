0095f20d54e0739fe428a3fda7ba5eb1e2056e69
Anjan Roy <anjanroy@yandex.com>
2020-08-07 11:16:33 +0530

    new: computes blocktime every 1 min

--- mgs/index.js
+++ mgs/index.js
@@ -1,6 +1,7 @@
 const dotnet = require('dotenv')
 const path = require('path')
 const Web3 = require('web3')
+const Recommendation = require('./recommendation');
 
 dotnet.config({ path: path.join(__dirname, '.env'), silent: true })
 
@@ -14,10 +15,18 @@ const getWeb3 = () => {
     return new Web3(new Web3.providers.WebsocketProvider(RPC))
 }
 
-const getBlockTime = async () => {
-    let latestBlock = await web3.eth.getBlock('latest')
-    let prevBlock = await web3.eth.getBlock(latestBlock.number - 1)
+const getBlockTime = async _web3 => {
+    let latestBlock = await _web3.eth.getBlock('latest')
+    let prevBlock = await _web3.eth.getBlock(latestBlock.number - 1)
     return latestBlock.timestamp - prevBlock.timestamp
 }
 
+const updateBlockTime = (_rec, _web3) => {
+    _rec.blockTime = getBlockTime(_web3)
+}
+
+
 const web3 = getWeb3()
+const recommendation = new Recommendation()
+
+setInterval(updateBlockTime, 60000, recommendation, web3)
