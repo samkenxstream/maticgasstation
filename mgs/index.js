402af72f26fef7fc1a47a994fce75f0acf117e64
Anjan Roy <anjanroy@yandex.com>
2020-08-07 09:24:58 +0530

    new: get web3 instance

--- mgs/index.js
+++ mgs/index.js
@@ -2,6 +2,7 @@
 
 const dotnet = require('dotenv');
 const path = require('path');
+const Web3 = require('web3');
 
 dotnet.config({ path: path.join(__dirname, '.env'), silent: true })
 
@@ -10,3 +11,9 @@ const STANDARD = process.env.STANDARD || 60;
 const FAST = process.env.FAST || 90;
 const RPC = process.env.RPC || 'wss://ws-mumbai.matic.today';
 const BUFFERSIZE = process.env.BUFFERSIZE || 500;
+
+const getWeb3 = () => {
+    return new Web3(new Web3.providers.WebsocketProvider(RPC));
+};
+
+const web3 = getWeb3();
