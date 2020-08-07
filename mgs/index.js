43ed6e61d5df594a0b4e4cc033da51ae7339fc2a
Anjan Roy <anjanroy@yandex.com>
2020-08-07 10:09:04 +0530

    fix: removed statement terminators

--- mgs/index.js
+++ mgs/index.js
@@ -1,25 +1,23 @@
-'use strict';
-
-const dotnet = require('dotenv');
-const path = require('path');
-const Web3 = require('web3');
+const dotnet = require('dotenv')
+const path = require('path')
+const Web3 = require('web3')
 
 dotnet.config({ path: path.join(__dirname, '.env'), silent: true })
 
-const SAFELOW = process.env.SAFELOW || 30;
-const STANDARD = process.env.STANDARD || 60;
-const FAST = process.env.FAST || 90;
-const RPC = process.env.RPC || 'wss://ws-mumbai.matic.today';
-const BUFFERSIZE = process.env.BUFFERSIZE || 500;
+const SAFELOW = process.env.SAFELOW || 30
+const STANDARD = process.env.STANDARD || 60
+const FAST = process.env.FAST || 90
+const RPC = process.env.RPC || 'wss://ws-mumbai.matic.today'
+const BUFFERSIZE = process.env.BUFFERSIZE || 500
 
 const getWeb3 = () => {
-    return new Web3(new Web3.providers.WebsocketProvider(RPC));
-};
-
-const getBlockTime = async () => {
-    let latestBlock = await web3.eth.getBlock('latest');
-    let prevBlock = await web3.eth.getBlock(latestBlock.number - 1);
-    return latestBlock.timestamp - prevBlock.timestamp;
+    return new Web3(new Web3.providers.WebsocketProvider(RPC))
 }
 
-const web3 = getWeb3();
+const getBlockTime = async () => {
+    let latestBlock = await web3.eth.getBlock('latest')
+    let prevBlock = await web3.eth.getBlock(latestBlock.number - 1)
+    return latestBlock.timestamp - prevBlock.timestamp
+}
+
+const web3 = getWeb3()
