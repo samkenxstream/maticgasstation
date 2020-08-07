d263f398fe8cb59f161ae066c420b838b58ac5ae
Anjan Roy <anjanroy@yandex.com>
2020-08-07 12:15:54 +0530

    new: processing blocks, getting gas prices

--- mgs/index.js
+++ mgs/index.js
@@ -12,6 +12,7 @@ const STANDARD = process.env.STANDARD || 60
 const FAST = process.env.FAST || 90
 const RPC = process.env.RPC || 'wss://ws-mumbai.matic.today'
 const BUFFERSIZE = process.env.BUFFERSIZE || 500
+const SINK = process.env.SINK || 'sink.json'
 
 const getWeb3 = () => {
     return new Web3(new Web3.providers.WebsocketProvider(RPC))
@@ -23,16 +24,52 @@ const getBlockTime = async _web3 => {
     return latestBlock.timestamp - prevBlock.timestamp
 }
 
-const updateBlockTime = (_rec, _web3) => {
+const updateBlockTime = (_web3, _rec) => {
     _rec.blockTime = getBlockTime(_web3)
 }
 
-const fetchBlockAndProcess = async _web3 => {
-    let latestBlock = _web3.eth.getBlock('latest');
+const processTransaction = async (_web3, _hash, _transactions) => {
+    let _transaction = await _web3.eth.getTransaction(_hash)
+    _transactions.add(new Transaction(_block.number, parseInt(_transaction.gasPrice, 10) / 1e9))
+}
+
+const processBlock = async (_web3, _transactions, _block) => {
+    if (_block.transactions.length == 0) {
+        return
+    }
+
+    for (let i = 0; i < _block.transactions.length; i++) {
+        await processTransaction(_web3, _block.transactions[i], _transactions)
+    }
+}
+
+const fetchBlockAndProcess = async (_web3, _transactions, _rec) => {
+    let latestBlock = await _web3.eth.getBlock('latest')
+
+    if (_transactions.all.length == 0) {
+        await processBlock(_web3, latestBlock, _rec)
+    } else {
+        if (_transactions.latestBlockNumber >= latestBlock.number) {
+            return
+        }
+
+        await processBlock(_web3, latestBlock, _rec)
+    }
+
+    const cumsumGasPrices = _transactions.cumulativePercentageOfGasPrices()
+
+    _rec.updateGasPrices(
+        _transactions.getMinGasPriceWithAcceptanceRateX(cumsumGasPrices, SAFELOW),
+        _transactions.getMinGasPriceWithAcceptanceRateX(cumsumGasPrices, STANDARD),
+        _transactions.getMinGasPriceWithAcceptanceRateX(cumsumGasPrices, FAST),
+        _transactions.getMinGasPriceWithAcceptanceRateX(cumsumGasPrices, 100)
+    )
+    _rec.blockNumber = _transactions.latestBlockNumber
 }
 
 
 const web3 = getWeb3()
 const recommendation = new Recommendation()
+const transactions = new Transactions(BUFFERSIZE)
 
-setInterval(updateBlockTime, 60000, recommendation, web3)
+setInterval(updateBlockTime, 60000, web3, recommendation)
