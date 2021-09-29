d8fdc82ef20543382a3d184ed1199c74b5299f17
Rajesh Chaganti <40339800+py-zoid@users.noreply.github.com>
2021-09-29 15:37:59 +0530

    Add health-check endpoint
    fixes:
    - all health check endpoint
    - handle incorrect endpoints error

--- mgs/serve.js
+++ mgs/serve.js
@@ -18,6 +18,27 @@ const runServer = _rec => {
         res.status(200).json(_rec.servable()).end()
     })
 
+    // healthcheck endpoint
+    app.get('/health-check', (req, res) => {
+      return res.status(200).json({ success: true, message: 'Health Check Success' })
+    })
+
+    // Invalid endpoint error handling
+    app.use((req, res, next) => {
+      const error = new Error('Not found')
+      error.status = 404
+      next(error)
+    })
+
+    app.use((error, req, res, next) => {
+      res.status(error.status || 500)
+      res.json({
+        error: {
+          message: error.message
+        }
+      })
+    })
+
     createServer(app).listen(port, host, _ => {
         console.log(`🔥 Listening at http://${host}:${port}`)
     })
